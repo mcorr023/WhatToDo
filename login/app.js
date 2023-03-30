64331a3e8c219f085abccec8a58a6b8e80b10c73
mcorr023 <mcorr023@fiu.edu>
2023-03-29 21:32:40 -0400

    major update

---
+++ login/app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const mysql = require("mysql");
+const doenv = require("dotenv");
+const path = require("path");
+const hbs = require("hbs");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+
+doenv.config({
+  path: "./.env",
+});
+const db = mysql.createConnection({
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASS,
+  database: process.env.DATABASE,
+});
+
+db.connect((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("MySQL Connection Success");
+  }
+});
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+
+//console.log(__dirname);
+const location = path.join(__dirname, "./public");
+app.use(express.static(location));
+app.set("view engine", "hbs");
+
+app.use("/", require("./pages"));
+app.use("/auth", require("./auth"));
+
+app.listen(5000, () => {
+  console.log("Server Started @ Port 5000");
+});
